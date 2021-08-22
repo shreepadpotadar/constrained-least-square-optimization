130e07653460ec24a1595f264123ba0e87525199
Shreepad Potadar <potadarshripad1@gmail.com>
2021-08-22 18:11:04 +0100

    Adding the header file which declares the LagMultEstimator.cpp

---
+++ include/LagMultEstimator.h
@@ -0,0 +1,27 @@
+#ifndef LAGMULTESTIMATOR_H
+#define LAGMULTESTIMATOR_H
+
+#pragma once
+
+#include <math.h>
+
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Core>
+
+
+class LagMultEstimator{
+ public:
+  //Overload constructor
+  LagMultEstimator( const Eigen::MatrixXd &U, const double s, const double atb );
+
+  double EstimateFunctionValue( const int k, const double lambda );
+
+  double FindOptimalLambda( double max_lambda, const int k, const double lambda_step, const double eps );
+
+  Eigen::MatrixXd mat_u;
+  double sigma;
+  double mat_atb_norm;
+
+};
+
+#endif
