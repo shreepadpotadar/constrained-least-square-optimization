39614a9b7db6e351a78936ced755c969a3e230e4
Shreepad Potadar <potadarshripad1@gmail.com>
2021-08-22 17:26:20 +0100

    Adding the Lancos.h header file

---
+++ include/Lanczos.h
@@ -0,0 +1,27 @@
+#ifndef LANCZOS_H
+#define LANCZOS_H
+
+#pragma once
+
+#include <iostream>
+
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Core>
+
+
+class Lanczos{
+ public:
+  //Overload constructor
+  Lanczos(const Eigen::MatrixXd &A, const Eigen::VectorXd b);
+
+  // member function that implements Lanczos matrix bidiagonalization algorithm
+  Eigen::MatrixXd Solution();
+
+  // matrix B stores the bidiagonalised version of matrix A
+  Eigen::MatrixXd mat_b;
+  Eigen::MatrixXd mat_a;
+  Eigen::VectorXd vec_b;
+};
+
+
+#endif
